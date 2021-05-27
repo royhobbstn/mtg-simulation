53566464315552dd033b9a876ba7da5d7230b02c
Daniel Trone <danieljtrone@gmail.com>
2021-05-27 06:25:24 -0500

    too much

--- data/cardData.js
+++ data/cardData.js
@@ -2,8 +2,25 @@ exports.cardData = [
   {
     id: 0,
     name: 'Mountain',
+    tapped: false,
+    playCost: null,
+    summoningSickness: false,
+    tapAction: [{ type: 'modifyMana', payload: { red: 1 } }],
+    tapCost: null,
+    type: 'basic land',
+    subtype: 'mountain',
   },
-  { id: 1, name: 'Lightning Bolt' },
+  {
+    id: 1,
+    name: 'Lightning Bolt',
+    tapped: false,
+    playCost: { red: 1 },
+    summoningSickness: false,
+    tapAction: null,
+    tapCost: null,
+    type: 'instant',
+    subtype: '',
+  },
 ];
 
 // disintegrate
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { cardData } = require('./data/cardData.js');
+const { v4: uuidv4 } = require('uuid');
 
 class Card {
   constructor(obj) {
-    this.id = obj.id;
-    this.name = obj.name;
+    for (let key of Object.keys(obj)) {
+      this[key] = obj[key];
+    }
   }
 }
 
@@ -17,7 +19,9 @@ class CardPool {
 
   getRandom() {
     const rnd = Math.floor(Math.random() * this.length);
-    return this.cards[rnd];
+    const card = JSON.parse(JSON.stringify(this.cards[rnd]));
+    card.uuid = uuidv4();
+    return card;
   }
 }
 
@@ -26,18 +30,18 @@ class Game {
     this.players = players;
     this.turn = 0; // index of player
     this.pool = new CardPool();
-    this.deckSize = 60;
+    this.libraryCardCount = 60;
   }
 
   getRandomPlayer() {
     return Math.floor(Math.random() * this.players.length);
   }
 
-  createDecks() {
-    for (let i = 0; i < this.deckSize; i++) {
+  createLibraries() {
+    for (let i = 0; i < this.libraryCardCount; i++) {
       for (let p = 0; p < this.players.length; p++) {
         const player = this.players[p];
-        player.deck.push(this.pool.getRandom());
+        player.library.push(this.pool.getRandom());
       }
     }
   }
@@ -47,7 +51,7 @@ class Game {
       const player = this.players[p];
       const drawCardsNumber = p === startingPlayerIndex ? 7 : 8;
       for (let i = 0; i < drawCardsNumber; i++) {
-        player.hand.push(player.deck.pop());
+        player.hand.push(player.library.pop());
       }
     }
   }
@@ -62,11 +66,41 @@ class Game {
     return playerCount > 1;
   }
 
+  getNextPlayerTurn() {
+    let updated = false;
+    do {
+      this.turn++;
+      if (this.turn >= this.players.length) {
+        this.turn = 0;
+      }
+      if (this.players[this.turn].life > 0) {
+        updated = true;
+      }
+    } while (updated === false);
+  }
+
+  playerTurn() {
+    // figure out who's turn it is next
+    this.getNextPlayerTurn();
+
+    const player = this.players[this.turn];
+
+    // start untap phase
+    player.board.untap(this.turn);
+    // start draw phase
+    player.drawPhase(this.turn);
+
+    // start main phase
+    player.mainPhase(this.turn);
+
+    player.discardPhase(this.turn);
+  }
+
   startGame() {
-    this.createDecks();
+    this.createLibraries();
     this.turn = this.getRandomPlayer();
     this.drawCards(this.turn);
-    // -------------
+    // -----game-loop--------
     let preventInfinite = 0;
     while (this.multiplePlayersAreStillAlive()) {
       preventInfinite++;
@@ -74,15 +108,106 @@ class Game {
         throw new Error('breaking infinite loop');
       }
       //
+      this.playerTurn();
     }
+  }
+}
+
+class Board {
+  constructor() {
+    this.lands = [];
+    this.creatures = [];
+    this.enchantments = [];
+    this.artifacts = [];
+  }
+
+  untap(playerId) {
+    this.lands.forEach(land => {
+      if (land.tapped) {
+        console.log(`${playerId}: untapped ${land.name}`);
+      }
+      land.tapped = false;
+    });
   }
 }
 
 class Player {
   constructor() {
     this.life = 20;
-    this.deck = [];
+    this.lostBecause = '';
+    this.library = [];
     this.hand = [];
+    this.graveyard = [];
+    this.exile = [];
+    this.board = new Board();
+    this.manaPool = { red: 0, blue: 0, black: 0, white: 0, green: 0, colorless: 0 };
+  }
+
+  discardPhase(playerId) {
+    if (this.life <= 0) {
+      return;
+    }
+
+    if (this.hand.length > 7) {
+      // DECISION POINT
+      // choose random card to discard
+      this.graveyard.push(this.hand.pop());
+      console.log(`${playerId}: discarded card`);
+    }
+  }
+
+  mainPhase(playerId) {
+    if (this.life <= 0) {
+      return;
+    }
+
+    // DECISION POINT
+    this.findRandomLandToPlay(playerId);
+
+    // DECISION POINT
+    this.findRandomCardToPlay(playerId);
+  }
+
+  findRandomCardToPlay(playerId) {
+    if (this.life <= 0) {
+      return;
+    }
+    //
+  }
+
+  findRandomLandToPlay(playerId) {
+    const card = this.hand.find(c => {
+      return c.type === 'basic land';
+    });
+    if (card) {
+      this.hand = this.hand.filter(c => {
+        return c.uuid !== card.uuid;
+      });
+      this.board.lands.push(card);
+      console.log(`${playerId}: playing ${card.name}`);
+    } else {
+      console.log(`${playerId}: no basic land to play`);
+    }
+  }
+
+  drawPhase(playerId) {
+    if (this.library.length === 0) {
+      this.life = 0;
+      this.lostBecause = 'Out of cards in library.';
+      console.log(`${playerId}: LOST: ${this.lostBecause}`);
+      return;
+    }
+    const newCard = this.library.pop();
+    console.log(`${playerId}: draws ${newCard.name}`);
+    this.hand.push(newCard);
+  }
+
+  modifyMana(manaObj) {
+    ['red', 'blue', 'black', 'white', 'green', 'colorless'].forEach(mana => {
+      if (manaObj[mana]) {
+        this.manaPool[mana] += manaObj[mana];
+      }
+    });
   }
 }
 
